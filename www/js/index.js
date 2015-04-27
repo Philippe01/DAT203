e317f4a5cebce07ba7b91d3f46bb54901f3b2255
Philippe01 <philippe.marr@gmail.com>
2015-04-27 16:57:55 +0100

    phonegap

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,8 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
 var pointarray, heatmap;
+
 var styles = [
   {
     "stylers": [
@@ -10,13 +14,14 @@ var styles = [
     ]
   }
 ];
+
 var mapOptions = {
   center: { lat: 50.3714, lng: -4.1422},
   zoom: 14,
   styles: styles
 };
-var map = new google.maps.Map(document.getElementById('map-canvas'),
-                              mapOptions);
+
+var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 var data;
 var heatmapData = [];
 var CrimesDataCat = [];
@@ -27,37 +32,22 @@ var userLocationLon = 0.0;
 var location_lat;
 var location_long;
 var Record_Coordinates = [];
-var route = [
-  new google.maps.LatLng(37.772323, -122.214897),
-  new google.maps.LatLng(21.291982, -157.821856),
-  new google.maps.LatLng(-18.142599, 178.431),
-  new google.maps.LatLng(-27.46758, 153.027892)
-];
+var route = [];
+
+/////// SOUNDS ////////
+var bass = document.createElement('audio');
+bass.setAttribute('src', 'wav/bassLouder.wav');
+bass.setAttribute('preload', 'auto');
+bass.setAttribute('loop', 'true');
+
+var publicOrderSound = document.createElement('audio');
+publicOrderSound.setAttribute('src', 'wav/deux.wav');
+publicOrderSound.setAttribute('preload', 'auto');
+
+
 function initialize() { 
-  console.log(location_lat +" "+ location_long);
-
-//  var styles = [
-//    {
-//      "stylers": [
-//        { "saturation": 22 },
-//        { "invert_lightness": true },
-//        { "lightness": 16 },
-//        { "weight": 2 },
-//        { "hue": "#00ccff" }
-//      ]
-//    }
-//  ];
-//
-
-//  var mapOptions = {
-//    center: { lat: 50.3714, lng: -4.1422},
-//    zoom: 14,
-//    styles: styles
-//  };
-//  map = new google.maps.Map(document.getElementById('map-canvas'),
-//                            mapOptions);
-
-
+//  console.log(location_lat +" "+ location_long);
+  
   var pointArray = new google.maps.MVCArray(heatmapData);
 
   heatmap = new google.maps.visualization.HeatmapLayer({
@@ -66,25 +56,33 @@ function initialize() {
 
   heatmap.setMap(map);
   
-
-
   var intervalID = window.setInterval(get_current_loc, 5000);
+  
+//  var bass = document.getElementById("bass");
+//  bass.play();
+  
+
+
+
+  
 }
 
 
 function get_current_loc() {
   
-  navigator.geolocation.getCurrentPosition(geolocationSuccess, onError);
+  var Options = { enableHighAccuracy: true };
+  
+  navigator.geolocation.getCurrentPosition(geolocationSuccess, onError, Options);
 
   function geolocationSuccess(position) {
 
     Record_Coordinates.push( position.coords.latitude, position.coords.longitude );
     route.push( new google.maps.LatLng( position.coords.latitude, position.coords.longitude ));
-    console.log( Record_Coordinates );
+//    console.log( Record_Coordinates );
 
     userLocationLat = Record_Coordinates[Record_Coordinates.length-2];
     userLocationLon = Record_Coordinates[Record_Coordinates.length-1];
-    console.log( userLocationLat + ', ' + userLocationLon );
+//    console.log( userLocationLat + ', ' + userLocationLon );
 
     document.getElementById("container").innerHTML = ""; 
 
@@ -100,19 +98,22 @@ function get_current_loc() {
         // Play sound files ///////////////////////////////////
         //////////////////////////////////////////////////////
         if(CrimesDataCat[arrayLoc] === "anti-social-behaviour" ) {       
-          document.getElementById('violinPlay').play();
+//          document.getElementById('violinPlay').play();
         } else if (CrimesDataCat[arrayLoc] === "shoplifting" ){
-          document.getElementById('bassPlay').play();
+//          document.getElementById('bassPlay').play();
+        } else if (CrimesDataCat[arrayLoc] === "public-order" ) {
+          publicOrderSound.play();  
         }
         //////////////////////////////////////////////////////
 
 
 
-        $("#container").append('<li id="list'+i+'" onclick="drag(list'+i+')" > <h3>' + CrimesDataCat[arrayLoc] + '</h3><p class="text" >'+ Description[arrayLoc] +'</p><p class="date">'+Date_of_Crimes[arrayLoc]+'</p></li>');
+        $("#container").append('<li id="list'+i+'" > <h3>' + CrimesDataCat[arrayLoc] + '</h3><p class="text" >'+ Description[arrayLoc] +'</p><p class="date">'+Date_of_Crimes[arrayLoc]+'</p></li>');
         //        console.log("close");
 
       }
     }
+    
     var path = new google.maps.Polyline({
       path: route,
       geodesic: true,
@@ -132,38 +133,13 @@ function get_current_loc() {
 
 function toggleHeatmap() {
   heatmap.setMap(heatmap.getMap() ? null : map);
+
+  bass.play();
 }
 
 
 function map_range(value, low1, high1, low2, high2) {
   return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
-
-function drag(item) {
-  var dragCounter = 0;
-  $(item).css("opacity", 1 );
-
-  $drag_counter_item = $( item );
-
-  $( item ).draggable({revert: true, axis: "x" });
-
-  $( item ).draggable({drag: function() {
-    dragCounter++;            
-    updateCounterStatus( $drag_counter_item, dragCounter );
-  }
-                      });
-
-}
-
-function updateCounterStatus( item, new_counter ) {
-  //    console.log(new_counter*5);
-  opacity_N  = map_range(new_counter*5, 0, 200, 1.0, 0.0);
-
-  $(item).css("opacity", opacity_N );
-  if (new_counter*5 > 200) {
-    $(item).remove();          
-  }
-
 }
 
 
@@ -222,7 +198,7 @@ function geolocationSuccess(position) {
   var location_lat = position.coords.latitude;
   var location_long = position.coords.longitude;
 
-  console.log(location_lat +" "+ location_long);
+//  console.log(location_lat +" "+ location_long);
 
   for (var i = 7; i <= 12; i++) {
     if (i < 10) {
@@ -245,45 +221,48 @@ function onError(error) {
 }
 
 
-getGeolocation();
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function onDeviceReady() {
+  
+  getGeolocation();
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+}
 
 
-// Swipe Pop-up Away
+google.maps.event.addListener(map, "click", function (event) {
 
+  userLocationLat = event.latLng.lat();
+  userLocationLon = event.latLng.lng();
+  console.log( userLocationLat + ', ' + userLocationLon );
 
+  document.getElementById("container").innerHTML = ""; 
 
+  for (var i = 0; i < heatmapData.length; i++) {
 
-//  google.maps.event.addListener(map, "click", function (event) {
-//    
-//    userLocationLat = event.latLng.lat();
-//    userLocationLon = event.latLng.lng();
-//    console.log( userLocationLat + ', ' + userLocationLon );
-//
-//    document.getElementById("container").innerHTML = ""; 
-//
-//    for (var i = 0; i < heatmapData.length; i++) {
-//
-//      if ((Math.abs(userLocationLat - heatmapData[i].k) < 0.0002) && (Math.abs(userLocationLon - heatmapData[i].D) < 0.0002))  {
-//        var arrayLoc = i        
-//
-//        console.log(CrimesDataCat[arrayLoc]);
-//
-//        if(CrimesDataCat[arrayLoc] === "anti-social-behaviour" ) {       
-//          document.getElementById('violinPlay').play();
-//        } else if (CrimesDataCat[arrayLoc] === "shoplifting" ){
-//          document.getElementById('bassPlay').play();
-//        }    
-//
-//        $("#container").append('<li id="list'+i+'" onclick="drag(list'+i+')" > <h3>' + CrimesDataCat[arrayLoc] + '</h3><p class="text" >'+ Description[arrayLoc] +'</p><p class="date">'+Date_of_Crimes[arrayLoc]+'</p></li>');
-//        //        console.log("close");
-//
-//      }
-//    }
-//
-//  });
+    if ((Math.abs(userLocationLat - heatmapData[i].k) < 0.0002) && (Math.abs(userLocationLon - heatmapData[i].D) < 0.0002))  {
+      var arrayLoc = i        
+
+      console.log(CrimesDataCat[arrayLoc]);
+
+      if(CrimesDataCat[arrayLoc] === "anti-social-behaviour" ) {       
+//        document.getElementById('violinPlay').play();
+      } else if (CrimesDataCat[arrayLoc] === "shoplifting" ){
+//        document.getElementById('bassPlay').play();
+      } else if (CrimesDataCat[arrayLoc] === "burglary" ) {
+        publicOrderSound.play();  
+      } 
+
+      $("#container").append('<li id="list'+i+'"> <h3>' + CrimesDataCat[arrayLoc] + '</h3><p class="text" >'+ Description[arrayLoc] +'</p><p class="date">'+Date_of_Crimes[arrayLoc]+'</p></li>');
+      //        console.log("close");
+
+    }
+  }
+
+});
+
 
 
 
